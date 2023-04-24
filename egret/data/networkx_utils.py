5d5938e5353b30aa14dd8e7e79807b11063957f8
Michael Bynum <michaelbynum@users.noreply.github.com>
2023-04-24 09:26:13 -0600

    improve graph utilities

--- egret/data/networkx_utils.py
+++ egret/data/networkx_utils.py
@@ -4,15 +4,25 @@ from pprint import pprint
 from .data_utils import zip_items
 
 
-def get_networkx_graph(model_data: ModelData) -> networkx.Graph:
+def get_networkx_graph(model_data: ModelData, handle_reverse_pairs='error') -> networkx.Graph:
+    assert handle_reverse_pairs in {'error', 'remove', 'keep'}
     model_data = model_data.clone_in_service()
     graph = networkx.Graph()
     buses = dict(model_data.elements(element_type='bus'))
     branch_attrs = model_data.attributes(element_type='branch')
     bus_pairs = zip_items(branch_attrs['from_bus'], branch_attrs['to_bus'])
-    unique_bus_pairs = dict((val, None) for idx, val in bus_pairs.items())
-    for fb, tb in unique_bus_pairs.keys():
-        assert (tb, fb) not in unique_bus_pairs
+
+    if handle_reverse_pairs == 'error':
+        unique_bus_pairs = dict((val, None) for idx, val in bus_pairs.items())
+        for fb, tb in unique_bus_pairs.keys():
+            assert (tb, fb) not in unique_bus_pairs
+    elif handle_reverse_pairs == 'remove':
+        unique_bus_pairs = dict()
+        for fb, tb in bus_pairs.values():
+            if (tb, fb) not in unique_bus_pairs:
+                unique_bus_pairs[(fb, tb)] = None
+    else:
+        unique_bus_pairs = dict((val, None) for idx, val in bus_pairs.items())
 
     for bus_name in buses.keys():
         graph.add_node(bus_name)
